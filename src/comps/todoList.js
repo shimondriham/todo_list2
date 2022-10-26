c6701675d2d0b49b409272276238e128ab60e74d
shimon driham <95618903+shimondriham@users.noreply.github.com>
2022-10-26 12:40:17 +0300

    todo_2

--- src/comps/todoList.js
+++ src/comps/todoList.js
@@ -1,12 +1,24 @@
 import React from 'react'
-
+import { useSelector,useDispatch } from 'react-redux';
+import { delSingleItem } from '../features/todoSlice';
 export default function TodoList() {
+  const dispatch = useDispatch();
+  const todos_ar = useSelector(state => state.todoSlice.todos_ar)
+
+
   return (
     <div className='col-md-7'>
-      <div className='border shadow-sm my-2 p-2'>
-        <button className='btn btn-danger float-end'>X</button>
-        <h3>Go to work - 13:00</h3>
-      </div>
+      {todos_ar.map(item => {
+        return (
+          <div key={item.id} className='border shadow-sm my-2 p-2'>
+            <button onClick={() => {
+              dispatch(delSingleItem({delId:item.id}))
+            }} className='btn btn-danger float-end' >X</button>
+            <h3>{item.name} - {item.time}</h3>
+          </div>
+        )
+      })}
+
     </div>
   )
 }
